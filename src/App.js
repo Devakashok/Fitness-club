cef0e52fedc0ce6b4a96293b1565c135d3aba9d7
Devakashok <devakashok2003@gmail.com>
2023-03-23 23:54:49 +0530

    All files merged

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 // import Hero from './components/Hero/Hero';
 import Plans from './components/Plans/Plans';
 import Testimonials from './components/Testimonials/Testimonials';
-
+import Footer from "./components/Footer/Footer";
+import Join from "./components/Join/Join";
 import Hero from './components/Hero/Hero';
 import Programs from './components/Programs/Programs';
 import Reasons from './components/Reasons/Reasons';
@@ -10,11 +11,13 @@ import Reasons from './components/Reasons/Reasons';
 function App() {
   return (
     <div className="App">
-      <Plans/>
-      <Testimonials/>
       <Hero/>
       <Programs/>
       <Reasons/>
+      <Plans/>
+      <Testimonials/>
+      <Join/>
+      <Footer/>
     </div>
   );
 }
