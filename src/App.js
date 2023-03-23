10508e0d667bee8e1efb19c9d4780d4f6a6b991c
Shreyasgokul41 <gokulr.20cse@kongu.edu>
2023-03-23 22:39:36 +0530

    Plans and testimonials added

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+// import Hero from './components/Hero/Hero';
+import Plans from './components/Plans/Plans';
+import Testimonials from './components/Testimonials/Testimonials';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Plans/>
+      <Testimonials/>
+    </div>
+  );
+}
+
+export default App;
