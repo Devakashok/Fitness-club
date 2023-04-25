4b551fbd34c53addb7c0aa5648e1b39bc98fb225
Devakashok <devakashok2003@gmail.com>
2023-04-25 10:28:52 +0530

    server

---
+++ client/src/data/testimonialsData.js
@@ -0,0 +1,25 @@
+import image1 from "../assets/t-image1.png";
+import image2 from "../assets/t-image2.jpg";
+import image3 from "../assets/t-image3.jpg";
+
+export const testimonialsData = [
+  {
+    image: image1,
+    review:
+      "I made the right choice by choosing Phoenix fitness club and by choosing the right plan and program I already achieved my ideal body!",
+    name: 'MATHEW HENDRICKSON',
+    status : 'ENTREPRENEUR'
+  },
+  {
+    image: image2,
+    review: 'The overall vibe is great and I have had nothing but good experiences. I definitely recommend them to my friends and family.',
+    name: 'JOHN KEVIN',
+    status: 'COACH'
+  },
+  {
+    image : image3,
+    review:'Gives me an excellent workout every time and is able to keep things fresh after months of going consistently. Great people and great results!',
+    name: 'FRANKLIN',
+    status: "CUSTOMER"
+  }
+];
